e16023d74f8dac0e5dfe087f7bc6a6f8bd6ac8a0
Singh <sansingh@C02Z13YGLVDQ.local>
2019-08-15 20:11:07 -0400

    day three

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 rm -r ./public
 gatsby build
 aws s3 rm s3://www.sanamdeep.com --recursive
-aws s3 sync ./build s3://www.sanamdeep.com
+aws s3 sync ./public s3://www.sanamdeep.com
 aws cloudfront create-invalidation --distribution-id E1DQJG1URGSL9M --paths "/*"
